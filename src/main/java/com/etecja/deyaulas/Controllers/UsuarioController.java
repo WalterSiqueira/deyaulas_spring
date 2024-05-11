b47844717099bbc9aeba6c7a190e17db82b25890
WalterSiqueira <walterbarbozasiqueira@gmail.com>
2024-05-11 19:04:00 -0300

    2 arquivos deletados

--- src/main/java/com/etecja/deyaulas/Controllers/UsuarioController.java
+++ src/main/java/com/etecja/deyaulas/Controllers/UsuarioController.java
@@ -3,28 +3,29 @@ package com.etecja.deyaulas.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import com.etecja.deyaulas.Entities.Usuario;
 import com.etecja.deyaulas.Repositories.UsuarioRepository;
 
 import ch.qos.logback.core.model.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @Controller
 @RequestMapping("/usuario")
-public class CriancaController {
+public class UsuarioController {
+    @SuppressWarnings("unused")
     @Autowired
-    private UsuarioRepository criancaRepository;
+    private UsuarioRepository UsuarioRepository;
 
     @GetMapping("/cadastro")
     public String CadastroUsuario(Model model) {
         return "CadastroUsuario";
     }
+
+    
 
     @GetMapping("/successo")
     public String successo() {
